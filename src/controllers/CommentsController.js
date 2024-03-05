58c411ae1b7ed539eab868707d3b7bcf815cc926
André <andregparada@gmail.com>
2024-03-05 13:38:00 -0300

    atualizado create e delete coments

--- src/controllers/CommentsController.js
+++ src/controllers/CommentsController.js
@@ -4,13 +4,10 @@ class CommentsController {
     async create(request, response) {
         const { content } = request.body;
         const { post_id } = request.params;
-        const [user] = await knex("posts").where({ id: post_id })
-        const user_id = String(user.id)
 
-        const [comment_id] = await knex("comments").insert({
+        await knex("comments").insert({
             content,
-            post_id,
-            user_id
+            post_id
         });
 
         response.json();
@@ -24,13 +21,13 @@ class CommentsController {
         return response.json(comment)
     }
 
-    // async delete(request, response) {
-    //     const { id } = request.params;
+    async delete(request, response) {
+        const { id } = request.params;
 
-    //     await knex("posts").where({ id }).delete();
+        await knex("comments").where({ id }).delete();
 
-    //     return response.json();
-    // }
+        return response.json();
+    }
 }
 
 module.exports = CommentsController;
--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -8,5 +8,6 @@ const commentsController = new CommentsController();
 
 commentsRoutes.post("/:post_id", commentsController.create);
 commentsRoutes.get("/:id", commentsController.show);
+commentsRoutes.delete("/:id", commentsController.delete);
 
 module.exports = commentsRoutes;
