5c75a81fc73846c9b2d5625318559f5979aa5c39
André <andregparada@gmail.com>
2024-03-05 13:49:00 -0300

    criado sessions controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 
 const usersRouter = require("./users.routes")
+const sessionsRouter = require("./sessions.routes")
 const postsRouter = require("./posts.routes")
 const commentsRouter = require("./comments.routes")
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/posts", postsRouter);
 routes.use("/comments", commentsRouter);
 
