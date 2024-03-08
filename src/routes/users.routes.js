f416ee50c95762d614901cc9a10a9786c09e84b8
André <andregparada@gmail.com>
2024-03-08 15:10:26 -0300

    adcionando middleware em users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,15 @@
 const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
-usersRoutes.get("/:id", usersController.show);
-usersRoutes.put("/:id", usersController.update);
-usersRoutes.delete("/:id", usersController.delete);
+usersRoutes.get("/:id", ensureAuthenticated, usersController.show);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.delete("/:id", ensureAuthenticated, usersController.delete);
 
 module.exports = usersRoutes;
